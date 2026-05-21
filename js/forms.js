c68e8cf1b6e3cae12b11e1789a66eda44d8f878c
Evangelista-dev <afonso.oliveira2301@gmail.com>
2026-05-21 09:30:02 -0300

    Atualização Final

---
+++ js/forms.js
@@ -0,0 +1,34 @@
+class contato {
+    constructor(nome, sobrenome, email, cpf, telefone, contato){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
+    }
+}
+
+function Post() {
+
+    let data = new contato(
+        document.getElementById("nomeid").value,
+        document.getElementById("sobrenomeid").value,
+        document.getElementById("emailid").value,
+        document.getElementById("cpfid").value,
+        document.getElementById("telefoneid").value,
+        document.querySelector(".aipapai").value
+    );
+
+    console.log("NOVO CADASTRO REGISTRADO:");
+    console.log(data);
+}
+
+function Enviar() {
+    var nome = document.getElementById("nomeid");
+    var sobrenome = document.getElementById("sobrenomeid");
+
+    if (nome.value != "" && sobrenome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value +' '+ sobrenome.value + '  os seus dados foram encaminhados com sucesso');
+    }
+}
